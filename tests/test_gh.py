783df96ca05ed68e005f184afcb4151c46f9ced6
Tom Barron <tusculum@gmail.com>
2019-11-21 08:22:25 -0500

    Test for gh __main__ function is_throw_away()

--- tests/test_gh.py
+++ tests/test_gh.py
@@ -1,12 +1,23 @@
+from gh import __main__ as ghm
 import pytest
 
 
 # -----------------------------------------------------------------------------
-def test_passes():
+@pytest.mark.parametrize("inp, exp", [
+    pytest.param("----------", True, id="throw away hyphen lines"),
+    pytest.param("", True, id="throw away empty lines"),
+    pytest.param("# this is a comment", True, id="throw away comment lines"),
+    pytest.param("-- DONE -----", True, id="-- DONE -----"),
+    pytest.param("- == DONE -----", True, id="- == DONE -----"),
+    pytest.param("=== DONE -----", True, id="=== DONE -----"),
+    pytest.param("everything else okay", False, id="everything else"),
+])
+def test_is_throw_away(inp, exp):
     """
-    Verifying that pytest works
+    Test function ghm.is_throw_away()
     """
-    assert 5 == 5
+    pytest.dbgfunc()
+    assert ghm.is_throw_away(inp) == exp
 
 
 # -----------------------------------------------------------------------------
